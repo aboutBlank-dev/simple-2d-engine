03892e709b2141f9637f5b3f0dd70951cba2849b
aboutBlank-dev <zemiguel99@hotmail.com>
2022-07-02 22:02:47 +0100

    added helper methods to vec 2 class

--- src/helper-classes.js
+++ src/helper-classes.js
@@ -3,4 +3,22 @@ export class Vector2 {
     this.x = x;
     this.y = y;
   }
+
+  magnitude() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  divide(n) {
+    if(n == 0) return new Vector2(0, 0);
+
+    return new Vector2(this.x / n, this.y / n);
+  }
+
+  multiply(n) {
+    return new Vector2(this.x * n, this.y * n);
+  }
+  
+  normalize() {
+    return this.divide(this.magnitude());
+  }
 }
