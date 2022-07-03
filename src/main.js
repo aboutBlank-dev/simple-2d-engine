b34fa76064594cf29266c70e43623aa7e33f988d
aboutBlank-dev <zemiguel99@hotmail.com>
2022-07-03 02:31:22 +0100

    re-named myCircle to mySquare accordingly

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 'use strict'
 import SimpleEngine from "./simple-2d-engine"
 import Square from "./Entities/square"
-import MyCircle from "./my-circle";
+import MySquare from "./my-square";
 import Input from "./input-manger";
 import Trapezium from "./Entities/trapezium";
 import Circle from "./Entities/circle";
@@ -9,6 +9,6 @@ import Circle from "./Entities/circle";
 const game = new SimpleEngine();
 
 game.entities.push(new Square(70, 200, 50, 50, 'red'))
-game.entities.push(new MyCircle(0, 0, 50, 'blue'))
+game.entities.push(new MySquare(0, 0, 50, 'blue'))
 //game.entities.push(new Trapezium(150, 200, 'green'))
 game.entities.push(new Circle(300, 100, 50, 'pink'))
